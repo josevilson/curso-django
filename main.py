a75986ef5c7d10aa1fd570ceefc107773a2de8a2
Jose Vilson <contatojosevilson@gmail.com>
2022-08-05 09:26:19 -0300

    testando

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('oi')
+print('hello world')
+
+
+class Agencia:
+    def __init__(self, cnpj, endereco):
+        self.cnpj = cnpj
+        self.endereco = endereco
+
+    def exibir_cnpj(self):
+        return print(self)
