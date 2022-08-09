6b2c5d91a7069da81a834e74d78292444ba07b95
Jose Vilson <contatojosevilson@gmail.com>
2022-08-08 23:49:55 -0300

    Testando 2 commit

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class Agencia:
     def __init__(self, cnpj, endereco):
         self.cnpj = cnpj
         self.endereco = endereco
+        self.numero = 52
+        self.github = "teste de commit"
 
     def exibir_cnpj(self):
         return print(self)
